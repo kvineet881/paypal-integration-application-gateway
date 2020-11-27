725edd30813175f7244bd3d827c65a5a65c7286b
Eureka server <kvineet881@gmail.com>
2020-11-27 18:20:48 +0530

    Paypal Integration is Completed

---
+++ src/main/java/com/altimetrik/paypal/application/paypalintegrationpaymentservice/PaypalIntegrationPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.paypal.application.paypalintegrationpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypalIntegrationPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaypalIntegrationPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/altimetrik/paypal/application/paypalintegrationpaymentservice/PaypalIntegrationPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.paypal.application.paypalintegrationpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypalIntegrationPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
